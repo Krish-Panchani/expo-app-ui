f8d7f4dd47ea95b02a0f2fb2f59bd4d5514e8724
Krish Panchani <krishpanchani1346@gmail.com>
2025-12-31 12:57:48 +0530

    Updated files for SEO

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Expo App UI – React Native Component Library</title>
+        <meta name="title" content="Expo App UI – React Native Component Library" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
